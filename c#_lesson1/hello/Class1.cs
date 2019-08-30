6f2798bb9ca67a1a4608a2d9a09aa163d0552eb9
Dima <dima.belemezov@gmail.com>
2019-08-30 13:35:19 +0300

    1st exercises

---
+++ c#_lesson1/hello/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        string name = Console.ReadLine();
+        Console.WriteLine("hello," + name);
+	}
+}
---
+++ c#_lesson1/hello/sayHello/personalHello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace sayHello
+{
+    class personalHello
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
